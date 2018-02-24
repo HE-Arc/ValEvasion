f847fda9c19f5a985f076a801e5ad02a272599cb
Gandus10 <laurent.gander@he-arc.ch>
2018-02-24 12:08:11 +0100

    create model for mail

--- valevasion/mail/models.py
+++ valevasion/mail/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Mail(models.Model):
+    receiver = models.EmailField()
+    sender = models.EmailField()
+    subject = models.TextField()
+    content = models.TextField()
